74fed6adddbd1f937292f0643cf7cd662a6ddd50
OlliK-dev <oltsuuka@gmail.com>
2026-05-13 11:37:53 +0300

    Implemented connection to Supabase, POST requests for sending words. Integrated more advanced UI features and behaviours to work with the selection of words and clearing the pallette when the POST request is fulfilled.

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -1,38 +1,91 @@
 import 'package:crossplatform2026project/app_state.dart';
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class FavoritesPage extends StatelessWidget {
+class FavoritesPage extends StatefulWidget {
   const FavoritesPage({super.key});
 
+  @override 
+  State<FavoritesPage> createState() => _FavoritesPageState();
+
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+  final selectedFavorites = <WordPair>{};
+  
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
     if (appState.favorites.isEmpty) {
-      return Center(child: Text('No favorites yet.'));
+      return Center(child: Text("No favorites yet."));
     }
 
-    return ListView(
+    selectedFavorites.removeWhere(
+      (pair) => !appState.favorites.contains(pair),
+    );
+
+    return ListView (
       children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text(
-            'You have '
-            '${appState.favorites.length} favorites:',
+        Padding(padding: const EdgeInsets.all(20),
+        child: ElevatedButton.icon(
+          onPressed: selectedFavorites.isEmpty
+          ? null
+          : () async {
+            final messenger = ScaffoldMessenger.of(context);
+            final word = selectedFavorites.toList();
+
+            try {
+              await appState.sendSelectedToSupabase(word);
+
+              setState(() {
+                selectedFavorites.clear();
+              });
+              
+              messenger.showSnackBar(
+                SnackBar(content: Text(
+                  "Sent ${word.length} favorites to Supabase"
+                ),
+                ),
+              );
+            } catch (error) {
+              messenger.showSnackBar(
+                SnackBar(content: Text(error.toString())),
+              );
+              debugPrint(error.toString());
+            }
+          },
+          icon: Icon(Icons.announcement_sharp),
+          label: Text("Send selected"),
           ),
         ),
         for (var pair in appState.favorites)
-          ListTile(
-            leading: IconButton(
-              icon: Icon(Icons.favorite),
+        CheckboxListTile(
+          value: selectedFavorites.contains(pair),
+          onChanged: (value) {
+            setState(() {
+              if (value == true) {
+                selectedFavorites.add(pair);
+              } else {
+                selectedFavorites.remove(pair);
+              }
+            });
+          },
+          secondary: IconButton(
+            icon: Icon(Icons.favorite),
             onPressed: () {
+              setState(() {
+                selectedFavorites.remove(pair);
+              });
               appState.removeFavorite(pair);
+              debugPrint("Removed from favorites");
             },
-            ),
-            title: Text(pair.asLowerCase),
           ),
+          title: Text(pair.asLowerCase),
+        ),
       ],
     );
+
   }
 }
